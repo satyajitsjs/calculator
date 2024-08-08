f57304b4e965aa94e8c736227cd9b43b47e28d91
satyajitsjs <satyajitsjs3@gmail.com>
2024-08-08 16:44:16 +0530

    CSRF

--- backend/settings.py
+++ backend/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 
 
-CORS_ALLOWED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     "https://calculator.alphazero.in"
 ]
